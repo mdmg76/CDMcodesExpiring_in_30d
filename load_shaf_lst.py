a35bc0ce85a7bd9960efaff694b9fe3364000216
mdmg76 <moody1976s@hotmail.com>
2024-09-06 15:39:55 +0400

    first commit

---
+++ load_shaf_lst.py
@@ -0,0 +1,56 @@
+import requests
+import pandas as pd
+import io
+from datetime import datetime
+from exchangelib import Credentials, Account, Message, Mailbox, HTMLBody, FileAttachment
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+load_dotenv(Path('.') / '.env')
+my_user = os.getenv('user_name')
+my_pass = os.getenv('password')
+
+credentials = Credentials(username=my_user, password=my_pass)
+account = Account('PharmacyDeptRAH@seha.ae', credentials=credentials, autodiscover=True)
+
+
+url = "https://shafafiyaportal.doh.gov.ae/dictionary/DrugCoding/Drugs.xlsx"
+
+r = requests.get(url, allow_redirects=True)
+xl = io.BytesIO(r.content)
+df = pd.read_excel(xl, sheet_name='Drugs')
+date_today = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+df['Today Date'] = date_today
+df['Today Date'] = pd.to_datetime(df['Today Date'])
+df['delta'] = (df['Today Date'] - df['Delete Effective Date']).dt.days
+pd.to_numeric(df['delta'])
+df = df[df['delta'].notna()]
+df = df.drop(df[(df.delta < -30.00)].index)
+df = df.drop(df[(df.delta >= 0.00)].index)
+df = df.drop(columns=['Today Date', 'delta'])
+
+df.to_excel('Expiring Grace Codes.xlsx', sheet_name='Drugs', index=False)
+
+
+# m = Message(
+#     account=account,
+#     subject='Shafafiya Codes Expiring in 30 Days',
+#     body=HTMLBody('''
+#     <html>
+#         <body style="font-family:Consolas; color:#0b3a5a">
+#             <p>Dear Colleagues,</p>
+#             <p>Kindly find the attached Shafafiya code expiring in the coming 30 days.</p>
+#             <p>Regards,</p>
+#         </body>
+#     </html>
+#     '''),
+#     to_recipients=[Mailbox(email_address='alhassan@seha.ae')
+#     ],
+
+#     cc_recipients=['mmughazi@seha.ae', 'ehekal@seha.ae'], 
+# )
+# my_file_1 = FileAttachment(name='Expiring Grace Codes.xlsx', content=open('Expiring Grace Codes.xlsx', 'rb').read())
+# m.attach(my_file_1)
+# m.send()
+# os.remove('Expiring Grace Codes.xlsx')
